d4d394198bc86e0767c11945ed46d36c2431d53a
nekkovo <lexie401djlm@gmail.com>
2023-10-05 20:27:29 -0600

    Initial commit

---
+++ Spinner.py
@@ -0,0 +1,24 @@
+
+import random
+
+class Spinner:
+    def __init__(self, synonym_file):
+        self.synonyms = self._load_synonyms(synonym_file)
+    
+    def _load_synonyms(self, synonym_file):
+        synonyms = {}
+        with open(synonym_file, 'r') as file:
+            for line in file:
+                word, syns = line.strip().split(':')
+                synonyms[word] = syns.split(',')
+        return synonyms
+    
+    def spin_text(self, text, probability):
+        words = text.split()
+        spun_words = []
+        for word in words:
+            if word in self.synonyms and random.randint(1, 100) > probability:
+                spun_words.append(random.choice(self.synonyms[word]))
+            else:
+                spun_words.append(word)
+        return ' '.join(spun_words)
