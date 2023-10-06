f5f5995d054a4138ade3f92d748605c6dddc6bea
nekkovo <lexie401djlm@gmail.com>
2023-10-05 20:37:02 -0600

    reformat the file

--- SpinFile.py
+++ SpinFile.py
@@ -1,14 +1,21 @@
+from Spinner import Spinner
+
 
 def preprocess_text(text):
     return ''.join(char.lower() if char.isalnum() or char.isspace() else ' ' for char in text)
 
+
 def main():
     with open('essay.txt', 'r') as file:
         original_text = preprocess_text(file.read())
-    
+
     spinner = Spinner('test-synonyms.txt')
-    
+
     print(f"Original: {original_text}")
     for i in range(3):
         spun_text = spinner.spin_text(original_text, 50)
         print(f"Option {i + 1}: {spun_text}")
+
+
+if __name__ == "__main__":
+    main()
